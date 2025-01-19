670c467ded79f42f3f9d0f05bf2129aee7107226
Qiqianily <Joedian@163.com>
2025-01-19 17:23:52 +0800

    add test for actions pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    println!("Hello Rust!")
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+    #[test]
+    fn test_it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 }
